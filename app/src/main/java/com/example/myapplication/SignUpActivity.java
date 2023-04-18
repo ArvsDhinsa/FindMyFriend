19ff0ccf38e570a7b46b8d9b8ab4319fd9e107ea
ArvsDhinsa <arvinddhinsa@gmail.com>
2023-04-18 18:53:31 +0100

    Signup Added

--- app/src/main/java/com/example/myapplication/SignUpActivity.java
+++ app/src/main/java/com/example/myapplication/SignUpActivity.java
@@ -1,10 +1,19 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -12,6 +21,10 @@ public class SignUpActivity extends AppCompatActivity {
     EditText editTextPassword;
     EditText editTextMobileNumber;
     EditText editTextEmail;
+
+    ProgressBar progressBar;
+
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +35,9 @@ public class SignUpActivity extends AppCompatActivity {
         editTextPassword = (EditText) findViewById(R.id.editTextPassword);
         editTextMobileNumber = (EditText) findViewById(R.id.editTextMobileNumber);
         editTextEmail = (EditText) findViewById(R.id.editTextEmail);
+        progressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mAuth = FirebaseAuth.getInstance();
     }
 
     public void signupButtonClicked(View v) {
@@ -50,5 +66,38 @@ public class SignUpActivity extends AppCompatActivity {
             editTextEmail.setError("Please enter an email address");
             editTextEmail.requestFocus();
         }
+
+        progressBar.setVisibility(View.VISIBLE);
+
+        mAuth.createUserWithEmailAndPassword(txtEmail, txtPassword)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            User user = new User(txtUserName, txtPassword, txtMobileNumber, txtEmail);
+
+                            FirebaseDatabase.getInstance().getReference("Users")
+                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                                    .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(SignUpActivity.this, "User registered successfully", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                            else {
+                                                Toast.makeText(SignUpActivity.this, "User failed to register", Toast.LENGTH_LONG).show();
+                                                progressBar.setVisibility(View.GONE);
+                                            }
+                                        }
+                                    });
+                        }
+                        else {
+                            Toast.makeText(SignUpActivity.this, "User failed to register", Toast.LENGTH_LONG).show();
+                            progressBar.setVisibility(View.GONE);
+                        }
+                    }
+                });
+
     }
 }
