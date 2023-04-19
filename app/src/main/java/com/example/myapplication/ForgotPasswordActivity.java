5151f9930a7db31fc1cf804b905547eb2b7dff28
ArvsDhinsa <arvinddhinsa@gmail.com>
2023-04-19 19:34:05 +0100

    Forgot Password function added

--- app/src/main/java/com/example/myapplication/ForgotPasswordActivity.java
+++ app/src/main/java/com/example/myapplication/ForgotPasswordActivity.java
@@ -1,14 +1,29 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.regex.Pattern;
 
 public class ForgotPasswordActivity extends AppCompatActivity {
 
     EditText editTextEmail;
+
+    FirebaseAuth mAuth;
+
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +32,38 @@ public class ForgotPasswordActivity extends AppCompatActivity {
 
         editTextEmail = (EditText) findViewById(R.id.editTextForgotPwdEmail);
 
+        mAuth = FirebaseAuth.getInstance();
+        progressBar = (ProgressBar) findViewById(R.id.forgotPasswordProgress);
+
     }
 
     public void forgotPasswordReset(View v) {
+        resetPassword();
+    }
 
+    private void resetPassword() {
+        String textEmail = editTextEmail.getText().toString().trim();
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(textEmail).matches()) {
+            editTextEmail.setError("Please enter your email address");
+            editTextEmail.requestFocus();
+            return;
+        }
+
+        progressBar.setVisibility(View.VISIBLE);
+        mAuth.sendPasswordResetEmail(textEmail).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(ForgotPasswordActivity.this, "Check your emails to reset your password", Toast.LENGTH_LONG).show();
+                    Intent intent = new Intent(ForgotPasswordActivity.this, LoginActivity.class);
+                    startActivity(intent);
+                }
+                
+                else {
+                    Toast.makeText(ForgotPasswordActivity.this, "Password reset failed", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 }
